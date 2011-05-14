a69a95bdfd5b04d56ec504bb8e1840b116a3cad8
Kennedyr <ekennedy50@gmail.com>
2011-05-14 17:25:51 -0400

    Problem 1

---
+++ Euler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Console.WriteLine( Problem1.SumTheMultiplesOf3And5(1000) );
+
+            sw.Stop();
+
+            Console.WriteLine("Time used (float): {0} ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Time used (rounded): {0} ms", sw.ElapsedMilliseconds);
+            
+        }
+    }
+}
---
+++ EulerUnitTests/Problem1Test.cs
@@ -0,0 +1,94 @@
+﻿using Euler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace EulerUnitTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for Problem1Test and is intended
+    ///to contain all Problem1Test Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class Problem1Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for SumTheMultiplesOf3And5
+        ///</summary>
+        [TestMethod()]
+        public void SumTheMultiplesOf3And5Test()
+        {
+            int limit = 10;
+            int expected = 23;
+            int actual;
+            actual = Problem1.SumTheMultiplesOf3And5(limit);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///A test for SumTheMultiplesOf3And5 speed
+        ///</summary>
+        [TestMethod()]
+        public void SumTheMultiplesOf3And5PerformanceTest()
+        {
+            int limit = 1000;
+            int time_limit = 10;//miliseconds
+            Stopwatch sw = Stopwatch.StartNew();
+            Problem1.SumTheMultiplesOf3And5(limit);
+            sw.Stop();
+            Assert.IsTrue(sw.Elapsed.TotalMilliseconds < time_limit);
+        }
+    }
+}
